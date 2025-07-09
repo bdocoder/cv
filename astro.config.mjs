f5942f498fd99b11eb7f09dcee024ed3e22e85c1
Abdullah Ahmad <bdocoder@gmail.com>
2025-07-09 23:38:19 +0300

    rewrite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,12 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss(), tsconfigPaths()],
+  },
+
+  integrations: [react()],
+});
--- scripts/build-cv.js
+++ scripts/build-cv.js
@@ -1,31 +1,26 @@
 import { chromium } from "playwright";
 import { preview } from "astro";
 
-async function run() {
-  const PORT = 4321;
-  const server = await preview({
-    root: ".",
-    server: {
-      port: PORT,
-    },
-    logLevel: "error",
-  });
+const PORT = 4321;
+const server = await preview({
+  root: ".",
+  server: {
+    port: PORT,
+  },
+  logLevel: "error",
+});
 
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto(`http://localhost:${PORT}`);
-  await page.emulateMedia({ media: "screen" });
-  await page.pdf({
-    path: "public/cv.pdf",
-    printBackground: true,
-    width: 1080,
-    margin: {
-      top: "24",
-    },
-  });
+const browser = await chromium.launch();
+const page = await browser.newPage();
+await page.goto(`http://localhost:${PORT}`);
+await page.emulateMedia({ media: "print" });
+await page.pdf({
+  path: "public/cv.pdf",
+  width: 1080,
+  margin: {
+    top: 24,
+  },
+});
 
-  await browser.close();
-  await server.stop();
-}
-
-run();
+await browser.close();
+await server.stop();
